73090ca07aef347d736c76865564ac4372252254
Matt Mower <mowerm@gmail.com>
2014-03-13 01:16:28 -0500

    overlays: Make it easier to override vendor/havoc
    There are two methods to overlay packages:
    DEVICE_PACKAGE_OVERLAYS
    PRODUCT_PACKAGE_OVERLAYS
    
    For each method, the first occurrence takes precedence, but also
    PRODUCT_ takes precedence to DEVICE_. Since vendor/cm applies to many
    devices, the values here sometimes need to be overridden. So that
    devices do not have to do any strange reorderings of inheritance (which
    can lead to problems if common_full_phone.mk in vendor/cm comes after
    full_base_telephony.mk, see http://review.cyanogenmod.org/59655), use
    DEVICE_ here and let devices use PRODUCT_.
    
    Change-Id: If1ab7729e39f9487bcc217e41d2dbfd1a6f7e5c2

--- products/common.mk
+++ products/common.mk
@@ -43,8 +43,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
     persist.service.adb.enable=1
 
 # Common overlay
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/havoc/overlay/common
+DEVICE_PACKAGE_OVERLAYS += vendor/havoc/overlay/common
 
 # Fix Dialer
 PRODUCT_COPY_FILES +=  \
