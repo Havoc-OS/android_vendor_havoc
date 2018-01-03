be83193cb98f84d97709d1cbee21cce4021691ba
maxwen <max.weninger@gmail.com>
2018-01-03 11:19:00 +0100

    Add OTA script do delete package cache [2/2]
    after flashing new image delete contents of /data/system/package_cache/
    to make sure they are recreated. Outdated cache files are the
    reason for the famous 'resource derps'

--- products/common.mk
+++ products/common.mk
@@ -98,6 +98,9 @@ PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/havoc/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions
 
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+
 # DU Utils Library
 PRODUCT_BOOT_JARS += \
     org.dirtyunicorns.utils
