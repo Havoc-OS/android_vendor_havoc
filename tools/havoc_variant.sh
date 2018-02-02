23f9ea0de124cce4098f8ff3f23bf512ea7902f1
westcripp <korukaltan@gmail.com>
2018-02-02 20:59:15 +0300

    vendor add build script

---
+++ tools/havoc_variant.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# Build type chooser for Havoc-OS
+# Specify colors utilized in the terminal
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    ylw=$(tput setaf 3)             #  yellow
+    blu=$(tput setaf 4)             #  blue
+    ppl=$(tput setaf 5)             #  purple
+    cya=$(tput setaf 6)             #  cyan
+    txtbld=$(tput bold)             #  Bold
+    bldred=${txtbld}$(tput setaf 1) #  red
+    bldgrn=${txtbld}$(tput setaf 2) #  green
+    bldylw=${txtbld}$(tput setaf 3) #  yellow
+    bldblu=${txtbld}$(tput setaf 4) #  blue
+    bldppl=${txtbld}$(tput setaf 5) #  purple
+    bldcya=${txtbld}$(tput setaf 6) #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+    rev=$(tput rev)                 #  Reverse color
+    pplrev=${rev}$(tput setaf 5)
+    cyarev=${rev}$(tput setaf 6)
+    ylwrev=${rev}$(tput setaf 3)
+    blurev=${rev}$(tput setaf 4)
+    normal='tput sgr0'
+
+echo -e ${ylw}"\n\n ▼ Which build-type do you want to choose?\n"${txtrst}
+echo -e "";
+echo -e ${blu}" 〉 1- Official "${txtrst}${red}"    ︱ Only for the official builds by the maintainers"${txtrst}
+echo -e "";
+echo -e ${blu}" 〉 2- Unofficial "${txtrst}${red}"  ︱ For the unofficial builds"${txtrst}
+echo -e "";
+echo -e ${blu}" 〉 3- Test "${txtrst}${red}"        ︱ Only for the test releases by the HAVOC-OS Team"${txtrst}
+echo -e "";
+echo -e ${blu}" 〉 4- Final "${txtrst}${red}"       ︱ Only for the final releases by the HAVOC-OS Team"${xtrst}
+echo -e "";
+echo -e ${blu}" 〉 30/sec Time-out"${txtrst}${red}" ︱ Default option"${txtrst}
+echo -e "";
+echo -e ${cya}" ▼ Pick a number"${txtrst}
+echo -e "";
+$normal
+read -t 30 askvariant
+sleep 1
+if [ "$askvariant" == "1" ]
+then
+echo -e "";
+echo -e ${blu}" ▪ Building official variant "${txtrst}
+export HAVOC_BUILDTYPE=Official
+echo -e "";
+echo -e "";
+$normal
+sleep 1
+fi
+if [ "$askvariant" == "2" ]
+then
+echo -e "";
+echo -e ${blu}" ▪ Building unofficial variant "${txtrst}
+export HAVOC_BUILDTYPE=Unofficial
+echo -e "";
+echo -e "";
+$normal
+sleep 1
+fi
+if [ "$askvariant" == "3" ]
+then
+echo -e "";
+echo -e ${blu}" ▪ Building test variant "${txtrst}
+export HAVOC_BUILDTYPE=Test
+echo -e "";
+echo -e "";
+$normal
+sleep 1
+fi
+if [ "$askvariant" == "4" ]
+then
+echo -e "";
+echo -e ${blu}" ▪ Building final variant "${txtrst}
+export HAVOC_BUILDTYPE=Final
+echo -e "";
+echo -e "";
+$normal
+sleep 1
+fi
