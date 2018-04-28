nitrogen-project/android_vendor_nitrogen

Add apn-conf.xml

products/common.mk
@@ -54,6 +54,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/nitrogen/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 
+# APN
+PRODUCT_COPY_FILES += \
+    vendor/nitrogen/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Extra packages
 PRODUCT_PACKAGES += \
     DaylightHeaderNitrogen \

Havoc-OS/android_vendor_havoc

products/common.mk
@@ -79,6 +79,10 @@ $(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size
 PRODUCT_COPY_FILES += vendor/havoc/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
 endif
 
+# APN
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Extra packages
 PRODUCT_PACKAGES += \
     LeanLauncher \