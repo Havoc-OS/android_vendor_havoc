44bc6ff089e0e35a77e73443b27dd6b83912bacb
Aaron Kling <webgeek1234@gmail.com>
2018-06-19 12:10:10 -0500

    ATV: Build LineageCustomizer
    Change-Id: I1a9e18516441dd513378bc5332b181b4e059e19b

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,6 +1,8 @@
 # Inherit full common Havoc stuff
 $(call inherit-product, vendor/havoc/config/common_full.mk)
 
-PRODUCT_PACKAGES += AppDrawer
+PRODUCT_PACKAGES += \
+    AppDrawer \
+    LineageCustomizer
 
 DEVICE_PACKAGE_OVERLAYS += vendor/havoc/overlay/tv
