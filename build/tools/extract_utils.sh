3fb459e208b5c78e50bb1ee37433cce9600e7ff4
Christopher N. Hesse <raymanfx@gmail.com>
2018-06-26 19:56:24 +0200

    verity_tool: Implement status getter
    VB1.0 and VB2.0 are handled.
    
    Change-Id: Id3753d3b7b87f3a8a2c03b96dd49be59b96e6d03

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -788,7 +788,7 @@ function oat2dex() {
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$LINEAGE_ROOT"/vendor/lineage/build/tools/"$HOST"/compact_dex_converter
+        export CDEXCONVERTER="$HAVOC_ROOT"/vendor/havoc/build/tools/"$HOST"/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder
