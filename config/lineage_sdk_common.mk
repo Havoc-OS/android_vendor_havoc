AfterlifeOS/vendor_afterlife

vendor: Copy org.lineageos.settings permission
Change-Id: Ia662f7789f77bb9b1abe1198db97a5b1767cd3e6

config/lineage_sdk_common.mk
@@ -4,6 +4,7 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.livedisplay.xml:system/etc/permissions/org.lineageos.livedisplay.xml \
     vendor/lineage/config/permissions/org.lineageos.performance.xml:system/etc/permissions/org.lineageos.performance.xml \
     vendor/lineage/config/permissions/org.lineageos.profiles.xml:system/etc/permissions/org.lineageos.profiles.xml \
+    vendor/lineage/config/permissions/org.lineageos.settings.xml:system/etc/permissions/org.lineageos.settings.xml \
     vendor/lineage/config/permissions/org.lineageos.style.xml:system/etc/permissions/org.lineageos.style.xml \
     vendor/lineage/config/permissions/org.lineageos.trust.xml:system/etc/permissions/org.lineageos.trust.xml \
     vendor/lineage/config/permissions/org.lineageos.weather.xml:system/etc/permissions/org.lineageos.weather.xml

Havoc-OS/android_vendor_havoc

config/lineage_sdk_common.mk
@@ -4,6 +4,7 @@ PRODUCT_COPY_FILES += \
     vendor/havoc/config/permissions/org.lineageos.livedisplay.xml:system/etc/permissions/org.lineageos.livedisplay.xml \
     vendor/havoc/config/permissions/org.lineageos.performance.xml:system/etc/permissions/org.lineageos.performance.xml \
     vendor/havoc/config/permissions/org.lineageos.profiles.xml:system/etc/permissions/org.lineageos.profiles.xml \
+    vendor/havoc/config/permissions/org.lineageos.settings.xml:system/etc/permissions/org.lineageos.settings.xml \
     vendor/havoc/config/permissions/org.lineageos.style.xml:system/etc/permissions/org.lineageos.style.xml \
     vendor/havoc/config/permissions/org.lineageos.trust.xml:system/etc/permissions/org.lineageos.trust.xml \
     vendor/havoc/config/permissions/org.lineageos.weather.xml:system/etc/permissions/org.lineageos.weather.xml