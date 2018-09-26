853b4b65d097e151a450264e446ac79c6487145f
oshmoun <oshmoun100@googlemail.com>
2018-09-27 02:35:33 +0530

    [2/2] Add a whitelist for location providers outside of /system
    Change-Id: I157bd5bcbba6f3355d09ef83a23f17d0d235a7a1

--- config/common.mk
+++ config/common.mk
@@ -44,6 +44,10 @@ else
     ro.device.cache_dir=/cache
 endif
 
+# Whitelist packages for location providers not in system
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.services.whitelist.packagelist=com.google.android.gms
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
