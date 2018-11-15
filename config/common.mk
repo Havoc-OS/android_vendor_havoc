d9d27e3b2a2beb3deb8c765975c1c01bbcf4b837
spezi77 <spezi7713@gmx.net>
2018-11-16 02:03:02 +0530

    Add 23 fancy accent colors [3/3]

--- config/common.mk
+++ config/common.mk
@@ -174,16 +174,21 @@ PRODUCT_PACKAGES += \
     Blue \
     BlueGrey \
     Brown \
+    CandyRed \
     Cyan \
     DeepOrange \
     DeepPurple \
+    ExtendedGreen \
     Green \
     Grey \
     Indigo \
+    JadeGreen \
     LightBlue \
     LightGreen \
     Lime \
     Orange \
+    PaleBlue \
+    PaleRed \
     Pink \
     Purple \
     Red \
@@ -196,7 +201,30 @@ PRODUCT_PACKAGES += \
     UserFour \
     UserFive \
     UserSix \
-    UserSeven
+    UserSeven \
+    ObfusBleu \
+    NotImpPurple \
+    Holillusion \
+    MoveMint \
+    FootprintPurple \
+    BubblegumPink \
+    FrenchBleu \
+    Stock \
+    ManiaAmber \
+    SeasideMint \
+    DreamyPurple \
+    SpookedPurple \
+    HeirloomBleu \
+    TruFilPink \
+    WarmthOrange \
+    ColdBleu \
+    DiffDayGreen \
+    DuskPurple \
+    BurningRed \
+    HazedPink \
+    ColdYellow \
+    NewHouseOrange \
+    IllusionsPurple
 
 # QS tile styles
 PRODUCT_PACKAGES += \
