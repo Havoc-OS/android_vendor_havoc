83d30a2ca3c999d7805bd90baf632ba40ae7afbb
Anushek Prasal <anushekprasal@gmail.com>
2018-12-17 03:13:25 +0530

    vendor: Add new props

--- config/version.mk
+++ config/version.mk
@@ -15,16 +15,20 @@ TARGET_PRODUCT_SHORT := $(subst havoc_,,$(HAVOC_BUILD_TYPE))
 
 # Set all versions
 DATE := $(shell date -u +%Y%m%d)
+HAVOC_BASE_SET_VERSION = 2.0
 HAVOC_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 HAVOC_BUILD_VERSION := Havoc-OS-$(HAVOC_BASE_VERSION)-$(DATE)-$(HAVOC_BUILD_TYPE)
-HAVOC_VERSION := Havoc-OS-$(HAVOC_BASE_VERSION)-$(DATE)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
+HAVOC_DATE := $(shell date -u +%d-%m-%Y)
 HAVOC_MOD_VERSION := Havoc-OS-$(HAVOC_BASE_VERSION)-$(HAVOC_BUILD_DATE)-$(HAVOC_BUILD_TYPE)
+HAVOC_VERSION := Havoc-OS-$(HAVOC_BASE_VERSION)-$(DATE)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
 ROM_FINGERPRINT := Havoc-OS/$(HAVOC_BASE_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(HAVOC_BUILD_DATE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
+    ro.havoc.base.version=$(HAVOC_BASE_SET_VERSION) \
+    ro.havoc.build.date=$(HAVOC_DATE) \
     ro.havoc.build.version=$(HAVOC_BUILD_VERSION) \
-    ro.havoc.version=$(HAVOC_VERSION) \
+    ro.havoc.fingerprint=$(ROM_FINGERPRINT) \
     ro.havoc.releasetype=$(HAVOC_BUILD_TYPE) \
-    ro.mod.version=$(HAVOC_MOD_VERSION) \
-    ro.havoc.fingerprint=$(ROM_FINGERPRINT)
+    ro.havoc.version=$(HAVOC_VERSION) \
+    ro.mod.version=$(HAVOC_MOD_VERSION)
