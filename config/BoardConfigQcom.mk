142e0bd0b173420f39c8edc568e5ea666f374e80
Max Weffers <rcstar6696@gmail.com>
2018-12-20 13:09:39 +0100

    havoc: config: fix include of qcom caf hals in soong namespace

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -80,9 +80,9 @@ endif
 endif
 
 PRODUCT_SOONG_NAMESPACES += \
-    hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT) \
-    hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT) \
-    hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT)
+    hardware/qcom/audio-caf-$(QCOM_HARDWARE_VARIANT) \
+    hardware/qcom/display-caf-$(QCOM_HARDWARE_VARIANT) \
+    hardware/qcom/media-caf-$(QCOM_HARDWARE_VARIANT)
 
 # QCOM HW crypto
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
