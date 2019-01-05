34d7eff8ad55c3f1e4b97e9b78e9e4e4c2daa845
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-05 11:46:23 +0100

    Disable art debug and enable minimize debug info
    Change-Id: I12b65c532201bf31f12cfb1d62c4a8bbe2204e2f

--- config/common.mk
+++ config/common.mk
@@ -117,6 +117,14 @@ PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 endif
 
+# Do not include art debug targets
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
 # Havoc Packages
 PRODUCT_PACKAGES += \
     Calculator \
