aefcf12446663a958ce76e7fa3f32bf1afb2766e
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-03 12:25:55 +0100

    Never allow neverallows
    Change-Id: Ibf07e9e319d676666e3e7389720c24917e38a9ac

--- build/core/config.mk
+++ build/core/config.mk
@@ -14,11 +14,6 @@
 # limitations under the License.
 #
 
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Rules for MTK targets
 include $(TOPDIR)vendor/havoc/build/core/mtk_target.mk
 
