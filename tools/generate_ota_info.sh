f4b5801f91bbbd0202e074ce50426183986b6fe8
Anushek Prasal <anushekprasal@gmail.com>
2019-03-06 21:30:55 +0100

    vendor: Generate file for OTA

---
+++ tools/generate_ota_info.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ "$1" ]
+then
+  file_path=$1
+  filename=$(basename "$file_path")
+  if [ -f $file_path ]; then
+    version=$(grep ro\.havoc\.base\.version $OUT/system/build.prop | cut -d= -f2);
+    romtype=$(echo $HAVOC_BUILD_TYPE);
+    size=$(stat -c%s $file_path);
+    datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
+    id=$(sha256sum $file_path | awk '{ print $1 }');
+    url="https://sourceforge.net/projects/havoc-os/files/$HAVOC_BUILD/$filename/download";
+    echo "{\n  \"response\": [\n    {\n      \"filename\": \"$filename\",\n      \"version\": \"v$version\",\n      \"romtype\": \"$romtype\",\n      \"size\": \"$size\",\n      \"datetime\": $datetime,\n      \"id\": \"$id\",\n      \"url\": \"$url\"\n    }\n  ]\n}" > $OUT/$HAVOC_BUILD.json
+  fi
+fi
