3944399745197c941f51ba93b250c04631dbadcd
Davide Garberi <dade.garberi@gmail.com>
2019-09-24 20:18:11 +0200

    prebuilt: Add a script to simplify A-only recovery system mount
    * This can be helpful because of the fragmentation that will happen with the system mount in recovery after Q
    
    Change-Id: I2d1e775efcf87e33319bc7790d1e54bca72116d3

--- config/common.mk
+++ config/common.mk
@@ -43,6 +43,10 @@ PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
 endif
 
+# system mount
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
+
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
     vendor/havoc/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml
