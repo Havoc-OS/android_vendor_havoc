d3e9567052ec75c8c26a432873978db25e9ced72
Anushek Prasal <anushekprasal@gmail.com>
2019-10-08 23:56:12 +0530

    Revert "build: Ignore neverallows"
    This reverts commit 84a5694fa28a35e3ce2b43a8625dd227057876d0.

--- build/core/config.mk
+++ build/core/config.mk
@@ -15,10 +15,5 @@
 
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/havoc/build/core/system_rro.mk
 
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Rules for QCOM targets
 include $(TOPDIR)vendor/havoc/build/core/qcom_target.mk
