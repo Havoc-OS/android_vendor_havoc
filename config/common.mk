b1be32ec4a984ec4151e1e08f5e5e997f3e2e912
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-10-09 15:30:24 +0200

    vendor: Kill sysinit
    Change-Id: Ia8d46d794ff829f3fc39ff48d57fdb6f9c03262d

--- config/common.mk
+++ config/common.mk
@@ -51,17 +51,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/havoc/config/permissions/havoc-sysconfig.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/havoc-sysconfig.xml
 
-# init.d support
-PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/etc/init.d/00banner:$(TARGET_COPY_OUT_SYSTEM)/etc/init.d/00banner \
-    vendor/havoc/prebuilt/common/bin/sysinit:$(TARGET_COPY_OUT_SYSTEM)/bin/sysinit
-
-ifneq ($(TARGET_BUILD_VARIANT),user)
-# userinit support
-PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/etc/init.d/90userinit:$(TARGET_COPY_OUT_SYSTEM)/etc/init.d/90userinit
-endif
-
 # Copy all Havoc-specific init rc files
 $(foreach f,$(wildcard vendor/havoc/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
