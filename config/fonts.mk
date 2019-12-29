4fa6efa25c76731410ec5ff6d5afda7f9e105c1f
Pranav Vashi <neobuddy89@gmail.com>
2019-12-29 17:27:38 +0530

    vendor: Add some fonts
    Co-authored-by: Lup Gabriel <gwolf2u.website@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,8 +1,2 @@
 PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
-    vendor/havoc/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
-    vendor/havoc/prebuilt/common/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
-    vendor/havoc/prebuilt/common/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/havoc/prebuilt/common/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/havoc/prebuilt/common/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    vendor/havoc/prebuilt/common/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
+    $(call find-copy-subdir-files,*,vendor/havoc/prebuilt/common/fonts,$(TARGET_COPY_OUT_SYSTEM)/fonts)
