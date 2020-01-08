ed39991a57c2594c747c224a2c3826741cae290b
Anushek Prasal <anushekprasal@gmail.com>
2020-01-09 02:37:24 +0530

    vendor: Havoc 3.1

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@ endif
 TARGET_PRODUCT_SHORT := $(subst havoc_,,$(HAVOC_BUILD_TYPE))
 
 # Set all versions
-HAVOC_VERSION = 3.0
+HAVOC_VERSION = 3.1
 HAVOC_BUILD_DATE := $(shell date -u +%d-%m-%Y)
 HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
 HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d)/$(shell date -u +%H%M)
