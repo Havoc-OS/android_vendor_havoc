8eb0b421c6a7d6b7b4c99317ab390e3e0290c3ed
Anushek Prasal <anushekprasal@gmail.com>
2020-01-27 17:09:05 +0530

    vendor: Convert NexusLauncherReleaseOverlay to a static overlay

--- packages/overlays/NexusLauncherReleaseOverlay/Android.mk
+++ packages/overlays/NexusLauncherReleaseOverlay/Android.mk
@@ -1,11 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_RRO_THEME := NexusLauncher
+
+LOCAL_PRODUCT_MODULE := true
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 LOCAL_PACKAGE_NAME := NexusLauncherReleaseOverlay
 LOCAL_SDK_VERSION := current
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := false
-LOCAL_PRODUCT_MODULE := true
-include $(BUILD_PACKAGE)
+
+include $(BUILD_RRO_PACKAGE)
