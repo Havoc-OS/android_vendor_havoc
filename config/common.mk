9519b514cb840a5ba3d2733ee76ad923976a4e16
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-01 19:54:23 -0300

    vendor: Add NexusLauncherRelease to PRODUCT_DEXPREOPT_SPEED_APPS
    Change-Id: I18db7cc09ca93bcbc268e5de7230117ead321634

--- config/common.mk
+++ config/common.mk
@@ -225,7 +225,8 @@ endif
 
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
+    SystemUI \
+    NexusLauncherRelease
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/havoc/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/havoc/overlay/common
