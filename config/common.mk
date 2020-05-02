f567617e2157afbff734ab4dd1a4e97ecf60757e
Anushek Prasal <anushekprasal@gmail.com>
2020-05-02 16:45:57 +0530

    vendor: Add GApps Gray Primary overlay

--- config/common.mk
+++ config/common.mk
@@ -156,6 +156,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     PrimaryColorCharcoalOverlay \
     PrimaryColorFlameOverlay \
+    PrimaryColorGrayOverlay \
     PrimaryColorNatureOverlay \
     PrimaryColorOceanOverlay
 
