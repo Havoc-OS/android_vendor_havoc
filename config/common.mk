16d0f2ce06d3c3af0421c86a54798fd4afe7365c
Pranav Vashi <neobuddy89@gmail.com>
2020-05-16 17:24:01 +0530

    vendor: Include faceunlock config if available

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,8 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
+$(call inherit-product-if-exists, external/motorola/faceunlock/config.mk)
+
 PRODUCT_BRAND ?= Havoc-OS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
