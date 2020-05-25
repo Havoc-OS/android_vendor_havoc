9a39dd7c83316e4ea8aaf0c551e380c785d1bd3a
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 18:08:31 +0800

    Export qcom UM hal root to soong
    Change-Id: Iabb75e8745dba9069c2eab0d5ef41b2171f4c048

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -9,6 +9,7 @@ $(call add_json_bool, Apply_msm8974_1440p_egl_workaround, $(filter true,$(TARGET
 $(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 $(call add_json_bool, Needs_camera_boottime_timestamp, $(filter true,$(TARGET_CAMERA_BOOTTIME_TIMESTAMP)))
+$(call add_json_str_omitempty, Qcom_um_soong_namespace, $(if $(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),$(QCOM_SOONG_NAMESPACE),))
 $(call add_json_bool, Should_wait_for_qsee, $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
 $(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_bool, Supports_extended_compress_format, $(filter true,$(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)))
