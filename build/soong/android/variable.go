AfterlifeOS/vendor_afterlife

Sort soong variables alphabetically
Change-Id: I880b07972fefb7fa2b40ab4aeacd073fb9a942b4

build/soong/android/variable.go
@@ -31,19 +31,19 @@ type Product_variables struct {
 	Supports_hw_fde_perf struct {
 		Cflags []string
 	}
+	Target_ignores_ftp_pptp_conntrack_failure struct {
+		Cppflags []string
+	}
 	Target_init_vendor_lib struct {
 		Whole_static_libs []string
+	}
+	Target_needs_netd_direct_connect_rule struct {
+		Cppflags []string
 	}
 	Target_process_sdk_version_override struct {
 		Cppflags []string
 	}
 	Target_shim_libs struct {
-		Cppflags []string
-	}
-	Target_needs_netd_direct_connect_rule struct {
-		Cppflags []string
-	}
-	Target_ignores_ftp_pptp_conntrack_failure struct {
 		Cppflags []string
 	}
 	Uses_generic_camera_parameter_library struct {
@@ -66,17 +66,17 @@ type ProductVariables struct {
 	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
 	Bootloader_message_offset  *int `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
 	Should_wait_for_qsee  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Supports_extended_compress_format  *bool `json:",omitempty"`
-	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
 	Supports_hw_fde  *bool `json:",omitempty"`
 	Supports_hw_fde_perf  *bool `json:",omitempty"`
+	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
+	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
-	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
-	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`

Havoc-OS/android_vendor_havoc

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

build/soong/android/variable.go
@@ -32,11 +32,17 @@ type Product_variables struct {
 	Supports_hw_fde_perf struct {
 		Cflags []string
 	}
+	Target_camera_needs_client_info struct {
+		Cflags []string
+	}
+	Target_ignores_ftp_pptp_conntrack_failure struct {
+		Cppflags []string
+	}
 	Target_init_vendor_lib struct {
 		Whole_static_libs []string
 	}
-	Target_camera_needs_client_info struct {
-		Cflags []string
+	Target_needs_netd_direct_connect_rule struct {
+		Cppflags []string
 	}
 	Target_process_sdk_version_override struct {
 		Cppflags []string
@@ -44,11 +50,8 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
-	Target_needs_netd_direct_connect_rule struct {
-		Cppflags []string
-	}
-	Target_ignores_ftp_pptp_conntrack_failure struct {
-		Cppflags []string
+	Target_uses_fod_hack struct {
+		Cflags []string
 	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
@@ -87,9 +90,6 @@ type Product_variables struct {
 		Header_libs []string
 		Shared_libs []string
 	}
-	Target_uses_fod_hack struct {
-		Cflags []string
-	}
 }
 
 type ProductVariables struct {
@@ -103,12 +103,13 @@ type ProductVariables struct {
 	Supports_extended_compress_format  *bool `json:",omitempty"`
 	Supports_hw_fde  *bool `json:",omitempty"`
 	Supports_hw_fde_perf  *bool `json:",omitempty"`
-	Target_init_vendor_lib  *string `json:",omitempty"`
 	Target_camera_needs_client_info  *bool `json:",omitempty"`
+	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
+	Target_init_vendor_lib  *string `json:",omitempty"`
+	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
-	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
-	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
+	Target_uses_fod_hack  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
@@ -118,5 +119,4 @@ type ProductVariables struct {
 	Uses_qcom_um_4_4_family  *bool `json:",omitempty"`
 	Uses_qcom_um_4_9_family  *bool `json:",omitempty"`
 	Uses_qcom_um_4_14_family  *bool `json:",omitempty"`
-	Target_uses_fod_hack  *bool `json:",omitempty"`
 }