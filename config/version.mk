1cd46c1b633948c8bcf25d396882fcec2c12347e
Anushek Prasal <anushekprasal@gmail.com>
2020-09-14 19:04:47 +0530

    vendor: Generate more data in OTA info script
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -18,13 +18,20 @@ HAVOC_BUILD_DATE := $(shell date -u +%d-%m-%Y)
 HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d)/$(shell date -u +%H%M)
 
 ifeq ($(WITH_GAPPS),true)
+    HAVOC_BUILD_VARIANT = GApps
     HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)-GApps
 else
+    HAVOC_BUILD_VARIANT = Vanilla
     HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
 endif
 
 export HAVOC_BUILD_DATE
 export HAVOC_BUILD_TYPE
+export HAVOC_BUILD_VARIANT
 export HAVOC_BUILD_VERSION
+export HAVOC_GROUP_URL
+export HAVOC_MAINTAINER
 export HAVOC_FINGERPRINT
 export HAVOC_VERSION
+export PRODUCT_BRAND
+export PRODUCT_MODEL
--- tools/generate_ota_info.sh
+++ tools/generate_ota_info.sh
@@ -4,12 +4,18 @@ then
   file_path=$1
   filename=$(basename "$file_path")
   if [ -f $file_path ]; then
+    oem=$(echo $PRODUCT_BRAND);
+    name=$(echo $PRODUCT_MODEL);
+    codename=$(echo $HAVOC_BUILD);
     version=$(echo $HAVOC_VERSION);
     romtype=$(echo $HAVOC_BUILD_TYPE);
+    variant=$(echo $HAVOC_BUILD_VARIANT);
+    maintainer=$(echo $HAVOC_MAINTAINER);
     size=$(stat -c%s $file_path);
     datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
     url="https://sourceforge.net/projects/havoc-os/files/$HAVOC_BUILD/$filename/download";
-    echo "{\n  \"response\": [\n    {\n      \"filename\": \"$filename\",\n      \"version\": \"$version\",\n      \"romtype\": \"$romtype\",\n      \"size\": \"$size\",\n      \"datetime\": $datetime,\n      \"id\": \"$id\",\n      \"url\": \"$url\",\n      \"changelog\": \"\"\n    }\n  ]\n}" > $OUT/$HAVOC_BUILD.json
+    group=$(echo $HAVOC_GROUP_URL);
+    echo "{\n  \"response\": [\n    {\n      \"oem\": \"$oem\",\n      \"name\": \"$name\",\n      \"codename\": \"$codename\",\n      \"filename\": \"$filename\",\n      \"version\": \"$version\",\n      \"romtype\": \"$romtype\",\n      \"variant\": \"$variant\",\n      \"maintainer\": \"$maintainer\",\n      \"size\": \"$size\",\n      \"datetime\": $datetime,\n      \"id\": \"$id\",\n      \"url\": \"$url\",\n      \"group\": \"$group\",\n      \"changelog\": \"\"\n    }\n  ]\n}" > $OUT/$HAVOC_BUILD.json
   fi
 fi
