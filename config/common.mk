242a88e924dd7e483e8fc7934770015b007430aa
Danny Lin <danny@kdrag0n.dev>
2020-11-06 21:40:29 -0800

    build: Set ro.apex.updatable=false in product properties
    Devices using prebuilt vendor images may set ro.apex.updatable=true in
    /vendor/build.prop, which causes boot failures because our APEXs are not
    built as updatable and thus never get mounted.
    
    Because init reads /product/build.prop *after* /vendor/build.prop and
    overrides read-only properties during early boot prop reading, we can
    set ro.apex.updatable=false explicitly in product properties to fix this
    issue.
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -105,6 +105,10 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # Flatten APEXs for performance
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
+# This needs to be specified explicitly to override ro.apex.updatable=true from
+# prebuilt vendors, as init reads /product/build.prop after /vendor/build.prop
+PRODUCT_PRODUCT_PROPERTIES += ro.apex.updatable=false
+
 # Bootanimation
 PRODUCT_PACKAGES += \
     bootanimation.zip
