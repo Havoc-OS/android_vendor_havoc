15457b4dca9a4f165f1c453fa902f207c00fe13c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-18 19:05:14 -0300

    vendor: Import props from pixel
    Change-Id: I146e94c5c9ff9441524b8b5628174313417f6e58
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/gapps.mk
+++ config/gapps.mk
@@ -1,6 +1,33 @@
 # GApps
 $(call inherit-product, vendor/gapps/config.mk)
 
+# Gboard configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
+
+# SetupWizard configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.enable_assist_gesture_training=true \
+    setupwizard.theme=glif_v3_light \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=false
+
+# StorageManager configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.storage_manager.show_opt_in=false
+
+# OPA configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true
+
+# Use gestures by default
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+
 # Overlays
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/havoc/overlay-pixel
 DEVICE_PACKAGE_OVERLAYS += vendor/havoc/overlay-pixel/common
