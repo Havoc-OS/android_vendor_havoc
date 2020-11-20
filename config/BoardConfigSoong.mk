f91533f81c31b3628cca445c271c615fb7f1591c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 12:50:43 -0300

    vendor: Allow forcing custom build fingerprint [1/2]
    Change-Id: I783a19829cb9d655f8abaeafa14f1f3d6a12ed20
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -42,6 +42,10 @@ SOONG_CONFIG_havocGlobalVars += \
     target_surfaceflinger_fod_lib \
     uses_camera_parameter_lib
 
+ifneq ($(TARGET_FORCE_BUILD_FINGERPRINT),)
+SOONG_CONFIG_havocGlobalVars += force_build_fingerprint
+endif
+
 SOONG_CONFIG_NAMESPACES += havocNvidiaVars
 SOONG_CONFIG_havocNvidiaVars += \
     uses_nv_enhancements
@@ -92,6 +96,11 @@ SOONG_CONFIG_havocGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_
 SOONG_CONFIG_havocGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_havocGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
 SOONG_CONFIG_havocGlobalVars_uses_camera_parameter_lib := $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)
+
+ifneq ($(TARGET_FORCE_BUILD_FINGERPRINT),)
+SOONG_CONFIG_havocGlobalVars_force_build_fingerprint := $(TARGET_FORCE_BUILD_FINGERPRINT)
+endif
+
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_havocQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
 endif
