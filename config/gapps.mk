28e53d2fcc29b7917e9ba78f37cb3ba64c54d481
Dyneteve <dyneteve@hentaios.com>
2020-12-13 00:57:11 +0900

    vendor: config: Build the textclassifier models
    Change-Id: I7cc85738a77498116a5ea6a30cffbce2eb163366
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/gapps.mk
+++ config/gapps.mk
@@ -41,6 +41,13 @@ PRODUCT_PACKAGES += \
     PixelLiveWallpapersOverlay
 endif
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+	libtextclassifier_annotator_en_model \
+	libtextclassifier_annotator_universal_model \
+	libtextclassifier_actions_suggestions_universal_model \
+	libtextclassifier_lang_id_model
+
 # RRO Overlays
 PRODUCT_PACKAGES += \
     FilesOverlay \
