yashsonawane/platform_vendor_lucid

vendor: Add fonts for customization and set Google Sans as default font.
Signed-off-by: Arseniy Graur <argraur@aosip.dev>

configs/common.mk
@@ -36,6 +36,9 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
+# Fonts
+include vendor/lucid/configs/fonts.mk
+
 # Common overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/lucid/overlay
 

Havoc-OS/android_vendor_havoc

Change-Id: Iac080faf905985c38bdb15caaa90cdc6ec8d909c
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

config/common.mk
@@ -261,6 +261,9 @@ ifeq ($(WITH_GAPPS),true)
 include vendor/gapps/config.mk
 endif
 
+# Fonts
+include vendor/havoc/config/fonts.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/havoc/config/partner_gms.mk
 -include vendor/havoc/config/version.mk