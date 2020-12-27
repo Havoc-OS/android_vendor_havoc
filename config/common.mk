PixelExperience/vendor_aosp

vendor: Cleanup makefiles
Change-Id: I8d05e229c0871ded085cfa0d469648f699f8fb01

config/common.mk
@@ -153,21 +153,21 @@ PRODUCT_PACKAGES += \
 #endif
 
 # Branding
-include vendor/aosp/config/branding.mk
+$(call inherit-product, vendor/aosp/config/branding.mk)
 
 # OTA
-include vendor/aosp/config/ota.mk
+$(call inherit-product, vendor/aosp/config/ota.mk)
 
 # GApps
-#include vendor/gapps/config.mk
+#$(call inherit-product, vendor/gapps/config.mk)
 
 # Audio
-include vendor/aosp/config/audio.mk
+$(call inherit-product, vendor/aosp/config/audio.mk)
 
 # Fonts
-include vendor/aosp/config/fonts.mk
+$(call inherit-product, vendor/aosp/config/fonts.mk)
 
 # CarrierSettings
-include vendor/aosp/CarrierSettings/config.mk
+$(call inherit-product, vendor/aosp/config/carrier_settings.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

Havoc-OS/android_vendor_havoc

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

config/common.mk
@@ -215,17 +215,17 @@ PRODUCT_PACKAGES += \
 
 # GApps
 ifeq ($(WITH_GAPPS),true)
-include vendor/gapps/config.mk
+$(call inherit-product, vendor/gapps/config.mk)
 endif
 
 # Audio
-include vendor/havoc/config/audio.mk
+$(call inherit-product, vendor/havoc/config/audio.mk)
 
 # Fonts
-include vendor/havoc/config/fonts.mk
+$(call inherit-product, vendor/havoc/config/fonts.mk)
 
 # RRO Overlays
-include vendor/havoc/config/rro_overlays.mk
+$(call inherit-product, vendor/havoc/config/rro_overlays.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/havoc/config/partner_gms.mk