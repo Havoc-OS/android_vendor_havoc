TheParasiteProject/vendor_parasite_common

vendor: Import overlays from RD1A.201105.003.C1
Change-Id: I1e698ee67e168dd56284e043cabb0544cb5f0b45

config/common.mk
@@ -104,8 +104,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aosp/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/aosp/overlay \
+    vendor/aosp/overlay-pixel \
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay/common \
+    vendor/aosp/overlay-pixel/common
 
 # TouchGestures
 PRODUCT_PACKAGES += \

Havoc-OS/android_vendor_havoc

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

config/common.mk
@@ -239,8 +239,14 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
     ShadyQuickStep
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/havoc/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/havoc/overlay/common
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/havoc/overlay \
+    vendor/havoc/overlay-pixel \
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/havoc/overlay/common \
+    vendor/havoc/overlay-pixel/common
 
 # Enable ccache
 USE_CCACHE := true