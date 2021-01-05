ad170242ae740fcc101b73466e3678f873517d6a
Anushek Prasal <anushekprasal@gmail.com>
2021-01-05 15:19:43 +0530

    vendor: soong: Add camera_needs_client_info
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,6 +31,7 @@ SOONG_CONFIG_NAMESPACES += havocGlobalVars
 SOONG_CONFIG_havocGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
@@ -67,6 +68,7 @@ SOONG_CONFIG_havocQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_havocGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_havocGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_havocGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_havocGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
