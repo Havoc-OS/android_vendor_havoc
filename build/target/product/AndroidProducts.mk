crdroidandroid/android_vendor_crdroid

vendor: targets: Add support for Legacy GSIs
* This is needed for VNDK27, and other older,
  hackier Treble implementations.

* We can't include `legacy_gsi_release.mk` as it's included
  in the `aosp_${ARCH}_ab` targets themselves, unlike the standard
  `aosp_${ARCH}` targets which guard `gsi_release.mk`'s inclusion
   with `aosp_${ARCH}` product guards.

Change-Id: I16c5983c18110b24eef84e382e0ae6852efb03c3
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

build/target/product/AndroidProducts.mk
@@ -19,6 +19,10 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_arm64.mk \
     $(LOCAL_DIR)/lineage_x86.mk \
     $(LOCAL_DIR)/lineage_x86_64.mk \
+    $(LOCAL_DIR)/lineage_arm_ab.mk \
+    $(LOCAL_DIR)/lineage_arm64_ab.mk \
+    $(LOCAL_DIR)/lineage_x86_ab.mk \
+    $(LOCAL_DIR)/lineage_x86_64_ab.mk \
     $(LOCAL_DIR)/lineage_tv_arm.mk \
     $(LOCAL_DIR)/lineage_tv_arm64.mk \
     $(LOCAL_DIR)/lineage_tv_x86.mk \
@@ -29,6 +33,10 @@ COMMON_LUNCH_CHOICES := \
     lineage_arm64-userdebug \
     lineage_x86-userdebug \
     lineage_x86_64-userdebug \
+    lineage_arm_ab-userdebug \
+    lineage_arm64_ab-userdebug \
+    lineage_x86_ab-userdebug \
+    lineage_x86_64_ab-userdebug \
     lineage_tv_arm-userdebug \
     lineage_tv_arm64-userdebug \
     lineage_tv_x86-userdebug \

Havoc-OS/android_vendor_havoc

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

build/target/product/AndroidProducts.mk
@@ -18,4 +18,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/havoc_arm.mk \
     $(LOCAL_DIR)/havoc_arm64.mk \
     $(LOCAL_DIR)/havoc_x86.mk \
-    $(LOCAL_DIR)/havoc_x86_64.mk
+    $(LOCAL_DIR)/havoc_x86_64.mk \
+    $(LOCAL_DIR)/havoc_arm_ab.mk \
+    $(LOCAL_DIR)/havoc_arm64_ab.mk \
+    $(LOCAL_DIR)/havoc_x86_ab.mk \
+    $(LOCAL_DIR)/havoc_x86_64_ab.mk \