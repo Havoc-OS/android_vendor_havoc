cc67891eff1e422a11a75ab0b2c174ea07e70256
Anushek Prasal <anushekprasal@gmail.com>
2021-04-30 18:20:13 +0530

    vendor: Fix launcher permissions
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -62,4 +62,15 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_DEX_PREOPT := false
 LOCAL_SYSTEM_EXT_MODULE := true
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3
+LOCAL_REQUIRED_MODULES += privapp_whitelist_com.android.launcher3-ext.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_com.android.launcher3-ext.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM_EXT_ETC)/permissions
+LOCAL_SYSTEM_EXT_MODULE := true
+LOCAL_SRC_FILES := etc/permissions/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
