6e33bca2f532a9c10ce8eb40dfee33ae54fa0c98
Anushek Prasal <anushekprasal@gmail.com>
2021-08-04 16:48:37 +0530

    vendor: Havoc 4.8
    Thanks to Shanky from https://t.me/LetItRipple for the wallpaper.
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -20,7 +20,7 @@ endif
 
 TARGET_PRODUCT_SHORT := $(subst havoc_,,$(HAVOC_BUILD_TYPE))
 
-HAVOC_VERSION = 4.6
+HAVOC_VERSION = 4.8
 HAVOC_BUILD_DATE := $(shell date -u +%d-%m-%Y)
 HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d)/$(shell date -u +%H%M)
 
