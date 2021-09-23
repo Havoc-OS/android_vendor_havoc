427054454e563c80d8ac8ef8e753ef33abb7e1cd
Anushek Prasal <anushekprasal@gmail.com>
2021-09-23 11:32:55 +0530

    vendor: Don't set maintainer props
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -1,15 +1,7 @@
 # Versioning System
 
 ifndef HAVOC_BUILD_TYPE
-HAVOC_BUILD_TYPE := Unofficial
-endif
-
-ifndef HAVOC_MAINTAINER
-HAVOC_MAINTAINER := Unknown
-endif
-
-ifndef HAVOC_GROUP_URL
-HAVOC_GROUP_URL := https://t.me/Havoc_OS
+    HAVOC_BUILD_TYPE := Unofficial
 endif
 
 # Only include Havoc OTA for official builds
@@ -25,9 +17,9 @@ HAVOC_BUILD_DATE := $(shell date -u +%d-%m-%Y)
 HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d)/$(shell date -u +%H%M)
 
 ifeq ($(WITH_GAPPS),true)
-HAVOC_BUILD_VARIANT = GApps
-HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)-GApps
+    HAVOC_BUILD_VARIANT = GApps
+    HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)-GApps
 else
-HAVOC_BUILD_VARIANT = Vanilla
-HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
+    HAVOC_BUILD_VARIANT = Vanilla
+    HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
 endif
