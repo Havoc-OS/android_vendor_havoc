eb4c0a9d000998628db50cd45f954f0b2db088f3
Arian <arian.kulmer@web.de>
2021-10-11 17:15:40 +0200

    config: Enable support of one-handed mode for phone targets
    Change-Id: Iab7de339e449d396d19783cfc254d540b600a33a
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -8,4 +8,8 @@ PRODUCT_PACKAGES += \
 # Include Havoc LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/havoc/overlay/dictionaries
 
+# Enable support of one-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
+
 $(call inherit-product, vendor/havoc/config/telephony.mk)
