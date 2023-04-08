8488a59740d1c9fa0834e4692848b785a7123a6b
Anushek Prasal <anushekprasal@gmail.com>
2023-04-09 01:43:50 +0530

    Import missing lineage overlay packages
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

---
+++ overlay/rro_packages/BlackTheme/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineageBlackTheme
+LOCAL_PRODUCT_MODULE := true
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineageBlackTheme
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)
