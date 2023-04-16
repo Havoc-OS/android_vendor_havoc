699dae3cf4a6a779d71536528023265e1597b0c7
Anushek Prasal <anushekprasal@gmail.com>
2023-04-16 19:21:23 +0530

    Add Face Unlock support
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/havoc.mk
+++ config/havoc.mk
@@ -90,3 +90,17 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Repainter integration
 PRODUCT_PACKAGES += \
     RepainterServicePriv
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+    PRODUCT_PACKAGES += \
+        ParanoidSense
+    PRODUCT_SYSTEM_EXT_PROPERTIES += \
+        ro.face.sense_service.enabled=true
+    PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+else
+    PRODUCT_PACKAGES += \
+        SettingsGoogleFutureFaceEnroll
+endif
